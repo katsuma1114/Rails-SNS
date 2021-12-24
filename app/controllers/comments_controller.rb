b01d53fcc4a1e5a9387a301a22b3691eecb59251
芝峰克昌 <katsuma.fishing@gmail.com>
2021-12-24 12:33:04 +0900

    コメント機能のデザインを整えた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,21 @@ class CommentsController < ApplicationController
     def index
     
         @article = Article.find(params[:article_id])
-        @comment = @article.comments.build #indexページでnewする
-
         @comments = @article.comments
+        
+        @comment = Comment.new
+
     end
 
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.build(comment_params)
         @comment.user = current_user
-        binding.pry
+
         if @comment.save
             redirect_to action: :index, notice: 'コメントを追加しました'
         else
-            flash.now[:error] = '追加できませんでした'
+
             render :index
         end
     end
