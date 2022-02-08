57cc6cc4bd4b80b715795bc3b132a696e13a4c2a
芝峰克昌 <katsuma.fishing@gmail.com>
2022-02-08 23:25:55 +0900

    heroku バグ修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,7 +22,7 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(:avatar)
+    params[:profile].permit(:avatar)
   end
 
 end
