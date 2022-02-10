467f810c51f2991960ae801131fbce6f98957b78
芝峰克昌 <katsuma.fishing@gmail.com>
2022-02-11 01:23:25 +0900

    jsをmoduleでリファクタ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,77 +15,36 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
-
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
-
-//フォローボタンの操作
-const handleFollowBtn = (hasFollowed) => {
-    if (hasFollowed) {
-        $('.btn-following').removeClass('hidden')
-      } else {
-        $('.btn-follow').removeClass('hidden')
-      }
-}
-
-// フォロワー数カウントアップ機能
-const followersUpCounter = () => {
-    const followerCount = Number($(".follower_count").text());
-    const countUp = followerCount + 1;
-    $(".follower_count").text(countUp);
-}
-
-// フォロワー数カウントダウン機能
-const followersDownCounter = () => {
-    const followerCount = Number($(".follower_count").text());
-    const countUp = followerCount - 1;
-    $(".follower_count").text(countUp);
-}
 
 document.addEventListener('DOMContentLoaded', () => {
+  //モーダル
+  $(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal,#modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+  });
 
-    const dataset = $('#account-show').data();
-    const accountId = dataset.accountId
+  //モーダル2
+  $('#openModal2').click(function(){
+    $('#modalArea2').fadeIn();
+  });
+  $('#closeModal2').click(function(){
+    $('#modalArea2').fadeOut();
+  });
 
-    axios.get(`/accounts/${accountId}/follows`)
-      .then((response) => {
-          const hasFollowed = response.data.followStatus
-          handleFollowBtn(hasFollowed)
-      })
-
-      //フォロー機能
-      $('.btn-follow').on('click', () => {
-        axios.post(`/accounts/${accountId}/follows`)
-          .then((response) => {
-            if (response.data.status === 'ok') {
-                followersUpCounter()
-
-                $('.btn-following').removeClass('hidden')
-                $('.btn-follow').addClass('hidden')
-            }
-          })
-          .catch((e) => {
-            window.alert('Error')
-            console.log(e)
-          })
-      })
-      
-      //アンフォロー機能
-      $('.btn-following').on('click', () => {
-        axios.post(`/accounts/${accountId}/unfollows`)
-          .then((response) => {
-            if (response.data.status === 'ok') {
-                followersDownCounter()
-
-                $('.btn-follow').removeClass('hidden')
-                $('.btn-following').addClass('hidden')
-            }
-          })
-          .catch((e) => {
-            window.alert('Error')
-            console.log(e)
-          })
-      })
+  //ツイッターにシェアするかどうかを確認（誤操作防止）
+  $('.share-twitter').click(function(){
+    if (!confirm('Twitterにシェアしますか？')) {
+      return false;
+    } else {
+      return true;
+    }
+  })
+  //avatar保存ボタン表示
+  $('.avatar-upload').click(function(){
+    $('.changed-file').removeClass('hidden')
+  })
 })
--- app/javascript/packs/follow.js
+++ app/javascript/packs/follow.js
@@ -1,26 +1,75 @@
-// import $ from 'jquery'
-// import axios from 'axios'
-// import { csrfToken } from 'rails-ujs'
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
 
 
-// axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
-// const handleFollowBtn = (hasFollowed) => {
-//     if (hasFollowed) {
-//         $('.btn-following').removeClass('hidden')
-//       } else {
-//         $('.btn-follow').removeClass('hidden')
-//       }
-// }
+//フォローボタンの操作
+const handleFollowBtn = (hasFollowed) => {
+    if (hasFollowed) {
+        $('.btn-following').removeClass('hidden')
+      } else {
+        $('.btn-follow').removeClass('hidden')
+      }
+}
 
-// document.addEventListener('DOMContentLoaded', () => {
+// フォロワー数カウントアップ機能
+const followersUpCounter = () => {
+    const followerCount = Number($(".follower_count").text());
+    const countUp = followerCount + 1;
+    $(".follower_count").text(countUp);
+}
 
-//     const dataset = $('#account-show').data();
-//     const accountId = dataset.accountId
+// フォロワー数カウントダウン機能
+const followersDownCounter = () => {
+    const followerCount = Number($(".follower_count").text());
+    const countUp = followerCount - 1;
+    $(".follower_count").text(countUp);
+}
 
-//     axios.get(`/accounts/${accountId}/follows`)
-//       .then((response) => {
-//           const hasFollowed = response.data.followStatus
-//           handleFollowBtn(hasFollowed)
-//       })
-// })
+document.addEventListener('DOMContentLoaded', () => {
+
+    const dataset = $('#account-show').data();
+    const accountId = dataset.accountId
+
+    axios.get(`/accounts/${accountId}/follows`)
+      .then((response) => {
+          const hasFollowed = response.data.followStatus
+          handleFollowBtn(hasFollowed)
+      })
+
+      //フォロー機能
+      $('.btn-follow').on('click', () => {
+        axios.post(`/accounts/${accountId}/follows`)
+          .then((response) => {
+            if (response.data.status === 'ok') {
+                followersUpCounter()
+
+                $('.btn-following').removeClass('hidden')
+                $('.btn-follow').addClass('hidden')
+            }
+          })
+          .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+          })
+      })
+      
+      //アンフォロー機能
+      $('.btn-following').on('click', () => {
+        axios.post(`/accounts/${accountId}/unfollows`)
+          .then((response) => {
+            if (response.data.status === 'ok') {
+                followersDownCounter()
+
+                $('.btn-follow').removeClass('hidden')
+                $('.btn-following').addClass('hidden')
+            }
+          })
+          .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+          })
+      })
+})
