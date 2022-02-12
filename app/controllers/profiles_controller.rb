5838bc09b6842e5a9de3c012074dfa5d40202790
芝峰克昌 <katsuma.fishing@gmail.com>
2022-02-12 13:50:00 +0900

    params デバック

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,8 @@ class ProfilesController < ApplicationController
   end
 
   def update
-    @profile = current_user.build_profile(profile_params)
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: 'プロフィール更新！'
     else
@@ -14,7 +15,7 @@ class ProfilesController < ApplicationController
       render :show
     end
   end
-
+  
   private
   def profile_params
     params.require(:profile).permit(:avatar)
