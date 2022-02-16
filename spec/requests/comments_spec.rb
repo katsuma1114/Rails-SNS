d126e068a3982cab9d44695dc790702d97636d43
芝峰克昌 <katsuma.fishing@gmail.com>
2022-02-16 14:19:54 +0900

    commentが保存できることを確認

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,14 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
+  let!(:user) { create(:user) }
+  let!(:article) { create(:article, user: user)}
+  let!(:comments) {create_list(:comment, 3, article: article, user: user)}
+  
   describe 'GET /comments' do
-    let!(:user) { create(:user) }
-    let!(:article) { create(:article, user: user)}
-    let!(:comments) {create_list(:comment, 3, article: article, user: user)}
     
     it '200ステータスが返ってくる' do
       get article_comments_path(article_id: article.id)
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /comments' do
+
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+
+      it "投稿が保存される" do
+        comment_params = attributes_for(:comment)
+        post article_comments_path( {article_id: article.id, comment: comment_params} )
+        expect(response).to have_http_status(302)
+        expect(Comment.last.content).to eq(comment_params[:content])
+      end
     end
   end
 end
